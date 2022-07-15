401ab099fdd96665997f454f3d6c18ae70e48113
shokri <elahe.es412@yahoo.com>
2022-07-15 14:49:01 +0430

    initail commit

---
+++ links/schema.py
@@ -0,0 +1,55 @@
+from dataclasses import fields
+import graphene
+from graphene_django import DjangoObjectType
+
+from .models import Link
+
+
+class LinkType(DjangoObjectType):
+    class Meta:
+        model = Link
+
+#1
+#  Defines a mutation class. Right after, you define the output of the mutation,
+#  the data the server can send back to the client. The output is defined field by field for learning purposes.
+#  In the next mutation you’ll define them as just one.
+class Query(graphene.ObjectType):
+    links = graphene.List(LinkType)
+
+    def resolve_links(self, info, **kwargs):
+        return Link.objects.all()
+
+
+class CreateLink(graphene.Mutation):
+    id = graphene.Int()
+    url = graphene.String()
+    description = graphene.String()
+
+    #2
+    # Defines the data you can send to the server, in this case, the links’ url and description.
+    class Arguments:
+        url = graphene.String()
+        description = graphene.String()
+
+    #3 resover
+    # The mutation method: it creates a link in the database using the data sent by the user,
+    #  through the url and description parameters.
+    #  After, the server returns the CreateLink class with the data just created.
+    #  See how this matches the parameters set on #1
+    def mutate(self, info, url, description):
+
+        link = Link(url=url, description=description)
+        link.save()
+
+        return CreateLink(
+            id=link.id,
+            url=link.url,
+            description=link.description,
+        )
+
+
+#4
+#Creates a mutation class with a field to be resolved, which points to our mutation defined before.
+
+class Mutation(graphene.ObjectType):
+    create_link = CreateLink.Field()
